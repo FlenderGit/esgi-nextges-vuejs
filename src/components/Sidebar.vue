85f0efbd46ea52ec319d1d84086c8df184842d52
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-11-29 22:50:26 +0100

    Up: New style for navbar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,13 +3,13 @@ import SidebarButton from './SidebarButton.vue'
 
 export default {
   components: {
-    SidebarButton
-  }
+    SidebarButton,
+  },
 }
 </script>
 
 <template>
-  <div class="h-screen bg-blue-900 px-3 py-2 flex flex-col gap-3">
+  <div class="h-screen bg-blue-900 px-3 py-2 flex flex-col gap-3 shadow-md shadow-neutral-800">
     <div class="round-full bg-red-600 size-12"></div>
     <nav class="grid gap-2">
       <SidebarButton icon="ic:baseline-edit-calendar" text="Home" />
