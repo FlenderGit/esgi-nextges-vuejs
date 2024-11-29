2ab8345e2d5357df82f8c6981d5452adcd2fc751
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-11-29 22:53:36 +0100

    ADD: Auth with firebase

---
+++ src/services/auth.ts
@@ -0,0 +1,67 @@
+import type { Credentials, RegisterCredentials } from '@/model/Credentials'
+import type { OAuth } from '@/model/Credentials'
+import type { Payload } from '@/stores/session'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+} from 'firebase/auth'
+
+const auth = getAuth()
+
+function register(credentials: RegisterCredentials) {
+  if (credentials.email && credentials.password) {
+    return createUserWithEmailAndPassword(auth, credentials.email, credentials.password)
+      .then((userCredential) => {
+        console.log('register', userCredential)
+        return userCredential.user.updateProfile({
+          displayName: credentials.username,
+        })
+      })
+      .catch((error) => {
+        console.error('register', error)
+      })
+  }
+}
+
+export async function firebase_login(credentials: Credentials): Promise<Payload> {
+  console.log('login', credentials)
+
+  if (credentials.email && credentials.password) {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        credentials.email,
+        credentials.password,
+      )
+
+      console.log('login', userCredential)
+
+      const accessToken = await userCredential.user.getIdToken()
+      const refreshToken = userCredential.user.refreshToken
+      const id = userCredential.user.uid
+      const email = userCredential.user.email
+      const username = userCredential.user.displayName
+
+      return {
+        user: {
+          id,
+          email,
+          username,
+        },
+        oauth: {
+          accessToken,
+          refreshToken,
+        },
+      }
+    } catch (error) {
+      console.error('login', error)
+      throw new Error('Authentication failed')
+    }
+  }
+
+  throw new Error('Invalid credentials')
+}
