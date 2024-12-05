d44fe190ea31bc8e4ede592406349fc4e64bbbcb
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-12-05 19:51:22 +0100

    ADD: Style on LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -39,10 +39,12 @@ export default {
 }
 </script>
 <template>
-  <p>Login</p>
-  <form>
-    <input type="text" placeholder="Username" v-model="email" />
-    <input type="password" placeholder="Password" v-model="password" />
-    <button @click.prevent="login">Login</button>
-  </form>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <form class="grid max-w-xs w-full gap-3 rounded-xl bg-sky-400/50 px-6 py-4 shadow-xl">
+      <h1 class="text-2xl font-bold text-center">Login</h1>
+      <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Username" v-model="email" />
+      <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="password" placeholder="Password" v-model="password" />
+      <button @click.prevent="login" class="rounded bg-cyan-900 text-neutral-50 py-1 hover:bg-cyan-800 transition-colors">Login</button>
+    </form>
+  </div>
 </template>
