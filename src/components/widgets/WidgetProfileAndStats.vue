373bca403c97a6270b1d8aa5f095c9fcc2543255
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-12-07 23:25:23 +0100

    ADD: Lower the font of profil

--- src/components/widgets/WidgetProfileAndStats.vue
+++ src/components/widgets/WidgetProfileAndStats.vue
@@ -14,22 +14,24 @@ export default {
     <WidgetBase title="Profile" :cols="2" :rows="2">
       <div class="flex flex-col gap-1">
         <div class="flex gap-2 items-center">
-          <div class="size-10 bg-neutral-100 rounded-full"></div>
+          <div class="size-12 bg-neutral-100 rounded-full"></div>
           <div class="flex flex-col">
             <p class="font-semibold text-lg">John Doe</p>
           <p class="text-neutral-600/80 text-sm">Informatique ESGI Mastère 3</p>
           </div>
         </div>
-        <div>
+        <div class="flex flex-col gap-1">
           <div class="flex justify-between">
-            <p class="text-neutral-700">Moyenne générale</p>
-            <p class="text-neutral-700">14.5/20</p>
+            <p class="text-neutral-700 text-sm">Moyenne générale</p>
+            <p class="text-neutral-700 text-sm">14.5/20</p>
           </div>
-          <progress class="w-full" value="14.5" max="20"></progress>
+          <div class="h-2 overflow-hidden w-full bg-neutral-100 rounded-full">
+            <div class="h-2 w-10/12 bg-sky-600"></div>
+          </div>
         </div>
         <div class="flex justify-between">
-          <p class="text-neutral-700">Absence</p>
-          <p class="text-neutral-700">3/20 autorisées</p>
+          <p class="text-neutral-700 text-sm">Absence</p>
+          <p class="text-neutral-700 text-sm">3/20 autorisées</p>
         </div>
       </div>
     </WidgetBase>
