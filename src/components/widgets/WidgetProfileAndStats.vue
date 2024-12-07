42ecab2d9c9c0bb3c97381fe43f10f49fca8096c
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-12-07 23:01:01 +0100

    ADD: Change the profil

--- src/components/widgets/WidgetProfileAndStats.vue
+++ src/components/widgets/WidgetProfileAndStats.vue
@@ -11,13 +11,26 @@ export default {
 </script>
 
 <template>
-      <WidgetBase title="Profile" :cols="3">
-        <div>Profile et stats, PP et Card number/stats
-          <div class="flex gap-2 justify-between">
-            <NumberStat title="Moyenne" :value="15.00" />
-            <NumberStat title="Devoirs" :value="2" />
+    <WidgetBase title="Profile" :cols="2" :rows="2">
+      <div class="flex flex-col gap-1">
+        <div class="flex gap-2 items-center">
+          <div class="size-10 bg-neutral-100 rounded-full"></div>
+          <div class="flex flex-col">
+            <p class="font-semibold text-lg">John Doe</p>
+          <p class="text-neutral-600/80 text-sm">Informatique ESGI Mastère 3</p>
           </div>
-          jean<br>jean<br>jean<br>jean<br>jean
         </div>
-      </WidgetBase>
+        <div>
+          <div class="flex justify-between">
+            <p class="text-neutral-700">Moyenne générale</p>
+            <p class="text-neutral-700">14.5/20</p>
+          </div>
+          <progress class="w-full" value="14.5" max="20"></progress>
+        </div>
+        <div class="flex justify-between">
+          <p class="text-neutral-700">Absence</p>
+          <p class="text-neutral-700">3/20 autorisées</p>
+        </div>
+      </div>
+    </WidgetBase>
 </template>
