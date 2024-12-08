702654595b43fb73c1f85b6849dc671c6b3273af
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-12-08 11:02:35 +0100

    UP: Change sidebar style

--- src/components/SidebarButton.vue
+++ src/components/SidebarButton.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="grid justify-items-center text-red-50 hover:bg-red-600 rounded transition-colors">
+  <div class="grid justify-items-center text-red-50 size-12 mx-auto hover:bg-cyan-300/20 rounded transition-colors" :title="text">
     <Icon :icon="icon" class="size-9" />
-    <p class="text-xs">Text</p>
+    <!-- <p class="text-xs">Text</p> -->
   </div>
 </template>
 
