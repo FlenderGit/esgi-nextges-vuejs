cea2ebf9cdf808dd9c1e8bdd38eee51330be5632
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-12-15 08:52:03 +0100

    UP: Change Person to add formation & scool

--- src/model/Person.ts
+++ src/model/Person.ts
@@ -1,10 +1,8 @@
 import type { Entity } from '@/services/Entity'
 
-export type UserData = Entity & {
-  username: string | null
-  email: string | null
-}
-
-export type User = UserData & {
-  courses: string[]
+export type User = Entity & {
+  name: string
+  email: string
+  formation: string
+  school: string
 }
