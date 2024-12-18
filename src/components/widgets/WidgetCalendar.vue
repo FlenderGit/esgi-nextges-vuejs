2f07ec37b2ec4e5a8751f0c8f86f0d1b8024a8af
delo0055 <tristan.deloeil@etudiant.univ-reims.fr>
2024-12-18 13:53:04 +0100

    UP: Use data from the database

--- src/components/widgets/WidgetCalendar.vue
+++ src/components/widgets/WidgetCalendar.vue
@@ -2,6 +2,8 @@
 import { defineComponent } from 'vue';
 import WidgetBase from './WidgetBase.vue';
 import { CalendarView } from "vue-simple-calendar"
+
+// Import the styles
 import "../../../node_modules/vue-simple-calendar/dist/style.css"
 import "../../../node_modules/vue-simple-calendar/dist/css/default.css"
 
@@ -15,13 +17,34 @@ export default defineComponent({
     WidgetBase,
     CalendarView,
   },
+  props: {
+    classes: {
+      type: Array as Class[],
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    items() {
+      if (!this.classes) return []
+      return this.classes.map((classe) => {
+        return {
+          id: classe.name,
+          startDate: new Date(classe.start * 1000),
+        }
+      })
+    }
+  }
 });
 </script>
 
 <template>
-  <WidgetBase title="Calendrier" :cols="2" :rows="3">
-    <div class="flex flex-col flex-grow">
-      <CalendarView :show-date="showDate" 			class="theme-default"/>
+  <WidgetBase title="Calendrier" :cols="2" :rows="2" :loading="loading">
+    <div v-show="!loading" class="flex flex-col flex-grow">
+      <CalendarView :items="items" :show-date="showDate" class="theme-default"/>
     </div>
   </WidgetBase>
 </template>
